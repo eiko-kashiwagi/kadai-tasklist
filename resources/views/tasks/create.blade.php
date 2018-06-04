dd15ebc72ac738ed3df7a490579e7131641bbe60
eiko.kashiwagi <ec2-user@ip-172-31-47-189.us-east-2.compute.internal>
2018-06-04 03:09:37 +0000

    design bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,7 +8,7 @@
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
      <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
     <div class="form-group">
@@ -21,7 +21,7 @@
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
                 
-                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+                {!! Form::submit('Add', ['class' => 'btn btn-primary']) !!}
         
             {!! Form::close() !!}
         </div>
