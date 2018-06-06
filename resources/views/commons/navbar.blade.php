6d9892e4b0a6abfa65580bcd60802bd99e6a6bf0
eiko.kashiwagi <ec2-user@ip-172-31-47-189.us-east-2.compute.internal>
2018-06-06 01:22:00 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,8 +12,8 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-th-large"></span>{!!link_to_route('tasks.index', 'HOME') !!}</li></button>
-                    <li><button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span>{!! link_to_route('tasks.create', 'Add New Tasks') !!}</li></button>
+                    <li><a href="#">Signup</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </div>
         </div>
